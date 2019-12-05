82ce148295d2139fea03db823b03d945bacd48da
Igor Nikolaev <igor.sv.n@gmail.com>
2019-12-05 08:37:40 +0300

    Replace deprecated code.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -25,8 +25,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        //$rootNode = $treeBuilder->root('darvin_fileman');
+        $treeBuilder = new TreeBuilder('darvin_fileman');
+//        $rootNode = $treeBuilder->getRootNode();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
